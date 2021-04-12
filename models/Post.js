3d99d5050c71dc951026d46c9258b5b6458b57a9
bishwavj <54392064+bishwavj@users.noreply.github.com>
2021-04-12 21:54:15 +0530

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -18,10 +18,6 @@ const postSchema = new Schema(
 			type: String,
 			required: true,
 		},
-		slug: {
-			type: String,
-			required: true,
-		},
 		userName: {
 			type: String,
 			required: true,
